2d253c61f31c72bd7d3cf615cede191342a02cfb
Devarsh <devarshraj5@gmail.com>
2022-04-25 18:21:04 +0530

    commit on master

--- day-8-cipher/cipher.py
+++ day-8-cipher/cipher.py
@@ -1,4 +1,5 @@
 def shift(s,k):
+ 
     nw = ""
     for i in s:
         
@@ -10,3 +11,21 @@ def shift(s,k):
 str = input("Enter the text: ")
 k = int(input("Enter the shift: "))
 print(shift(str,k))
+
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
