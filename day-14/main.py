5a1f430f0d8307361f525e23e0caded50cb21d92
Devarsh Raj <devarsh.raj_cs19@gla.ac.in>
2022-05-02 19:21:27 +0530

    turtle graphics

---
+++ day-14/main.py
@@ -0,0 +1,92 @@
+from re import S
+from secrets import choice
+from turtle import *
+from random import randint
+
+
+tim = Turtle()
+
+tim.shape("turtle")
+tim.color("red")
+
+# #RECTANGLE
+
+# for x in range(4):
+#     tim.forward(100)
+#     tim.left(90)
+
+# ----------------------------------------------------------------------------
+
+
+# #DASHED LINE
+
+# for x in range(20):
+#     tim.forward(5)
+#     tim.pu()
+#     tim.forward(5)
+#     tim.pd()
+# ------------------------------------------------------
+
+# # SHAPES
+
+# tim.hideturtle()
+
+# colors = ["red", "orange", "pink", "black", "blue", "cyan", "green"]
+
+# s = 3
+
+# for i in range(9):
+#     tim.color(choice(colors))
+#     for y in range(s):
+#         tim.forward(100)
+#         tim.left(360/s)
+        
+#     s +=1
+
+
+
+# -------------------------------------------------
+
+# # RANDOM WALK
+
+# tim.hideturtle()
+# colors = ["red", "orange", "pink", "black", "blue", "cyan", "green", "brown", "purple"]
+# tim.pensize(width=4)
+# tim.speed(0)
+
+# direction = [0 , 90, 180, 270]
+
+# for i in range(200):
+#     tim.forward(30)
+#     tim.setheading(choice(direction))
+#     tim.color(choice(colors))
+
+
+# -------------------------------------------------
+def gen_color():
+    r = randint(0, 255)
+    g = randint(0, 255)
+    b = randint(0, 255)
+    tp = (r, g,b)
+    return tp
+    
+
+tim.hideturtle()
+colors = ["red", "orange", "pink", "black", "blue", "cyan", "green", "brown", "purple"]
+tim.pensize(width=4)
+tim.speed("fastest")
+tim.colormode(255)
+
+direction = [0 , 90, 180, 270]
+
+for i in range(200):
+    tim.forward(30)
+    tim.setheading(choice(direction))
+    tim.color(gen_color())
+
+
+
+    
+    
+
+exitonclick()
