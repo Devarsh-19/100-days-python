b6cf6d981641db96289ee22a26ae7e5b30642e41
Devarsh-19 <devarshraj5@gmail.com>
2022-06-14 13:19:50 +0530

    committed on 14-06-2022

--- day-18-pong/main.py
+++ day-18-pong/main.py
@@ -8,4 +8,13 @@ screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("PONG")
 
+
+paddle = Turtle()
+paddle.shape('square')
+paddle.color('white')
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.pu()
+paddle.goto(350, 0)
+
+
 screen.exitonclick()
